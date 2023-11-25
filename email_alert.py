0b63a71fa03f1ffdf65680afd3352cd74a174668
kyunghwanleethebest <khrock1@gmail.com>
2023-11-26 03:16:37 +0900

    finish the code

--- email_alert.py
+++ email_alert.py
@@ -1,10 +1,9 @@
 import getpass
 import smtplib
 from email.message import EmailMessage
-import logging
 import datetime
 
-def alert_mail(EMAIL_ADDR : str = None, EMAIL_PASSWORD : str = None, author : str = 'Unknown'):
+def alert_mail(EMAIL_ADDR : str = None, EMAIL_PASSWORD : str = None, articles : dict = {}):
     try:
         SMTP_SERVER = 'smtp.gmail.com'
         SMTP_PORT = 465
@@ -15,10 +14,14 @@ def alert_mail(EMAIL_ADDR : str = None, EMAIL_PASSWORD : str = None, author : st
         smtp.login(EMAIL_ADDR, EMAIL_PASSWORD)
 
         message = EmailMessage()
-        content = f'{author}s new material is detected - {datetime.datetime.now()} '
+        content = f'New Important article title : \n'
+        for title, authors in articles.items():
+            content+=(title+'\n')
+            content+=(', '.join(authors)+'\n\n\n')
+        #
         message.set_content(content)
 
-        message["Subject"] = f"New Material Alert by {author}"
+        message["Subject"] = f"New Material is detected - {datetime.datetime.now()}"
         message["From"] = EMAIL_ADDR 
         message["To"] = EMAIL_ADDR
 
--- execute.py
+++ execute.py
@@ -1,64 +1,91 @@
-from url import arxiv_connect
 from email_alert import alert_mail
-import json
-import os
 import logging
-import schedule, keyboard
+import schedule
 import getpass
+import requests
+from bs4 import BeautifulSoup
+from collections import OrderedDict
 
 
-def main(*args):
-    # print(args)
-    author_name, g_mail_id, g_mail_pwd = args
+def arxiv_connect(authors : list = [], EMAIL_ADDR : str = None, EMAIL_PASSWORD : str = None):
+    if not authors:
+        logging.info('No authors input!!!')
+        return 
 
-    author_name_paper_number, author_search_paper_number = arxiv_connect(author=author_name)
-    logging.info(f'Searching for author {author_name}')
+    url = 'https://arxiv.org/list/cond-mat/new'
 
-    if not os.path.isfile('author_material.json'):
-        author_info = dict()
-        author_info[author_name] = {'num_of_paper':author_name_paper_number, 'num_of_search_res_paper': author_search_paper_number}
-        json_data = json.dumps(author_info, indent=4)
+    req = requests.get(url)
+    html = req.text
 
-        with open('author_material.json', 'w') as f:
-            f.write(json_data)
-            logging.info(f"Add Material Info of new author : {author_name}")
+    soup = BeautifulSoup(html,'html.parser')
 
-    else:
-        with open('author_material.json', 'r') as file:
-            author_info = json.load(file)
 
-            if author_name not in author_info:
-                author_info[author_name] = {'num_of_paper':author_name_paper_number, 'num_of_search_res_paper': author_search_paper_number}
-                json_data = json.dumps(author_info, indent=4)
-                with open('author_material.json', 'w') as f:
-                    f.write(json_data)
-                    logging.info(f"Add Material Info of new author : {author_name}")
-            
+    article_titles = OrderedDict()
+
+    
+    section_hash = {'New submissions':'10',
+                    # , 'Cross-lists':'12', 
+                    'Replacements':'14'
+                    }
+    
+    for sector in section_hash.keys():
+        num_of_article = 1
+        while True:
+            contents = soup.select(f'#dlpage > dl:nth-child({section_hash[sector]}) > dd:nth-child({int(num_of_article * 2)})')
+
+            if not contents:
+                break
             else:
-                if author_info[author_name]['num_of_paper'] != author_name_paper_number:
+                author_number = 1
+                this_paper_authors = []
+                article_title = 'None'
+                while True:
+                    ca = soup.select(f'#dlpage > dl:nth-child({section_hash[sector]}) > dd:nth-child({int(num_of_article * 2)}) > div > div.list-authors > a:nth-child({author_number+1})')
 
-                    alert_mail(EMAIL_ADDR=g_mail_id, EMAIL_PASSWORD=g_mail_pwd,author=author_name)
-                    logging.info(f"Detect New Material from author : {author_name}")
+                    if not len(ca):
+                        break
+                    content_author = soup.select(f'#dlpage > dl:nth-child({section_hash[sector]}) > dd:nth-child({int(num_of_article * 2)}) > div > div.list-authors > a:nth-child({author_number+1})')[0].get_text()
+                    real_name = content_author
+                    content_author = content_author.replace(',', '').replace('.', '')
+                    
+                    this_paper_authors.append(real_name)
+                    logging.info(f'Searching for {sector} content number : {num_of_article}  , author {content_author}, {author_number}')
 
-                    author_info[author_name] = {'num_of_paper':author_name_paper_number, 'num_of_search_res_paper': author_search_paper_number}
-                    json_data = json.dumps(author_info, indent=4)
-                    with open('author_material.json', 'w') as f:
-                        f.write(json_data)
-    return 
+                    if content_author in authors:
+                        article_title = soup.select(f'#dlpage > dl:nth-child({section_hash[sector]}) > dd:nth-child({int(num_of_article * 2)}) > div > div.list-title.mathjax')[0].get_text()
+                        if article_title not in article_titles:
+                            article_titles[article_title] = None
+                    
+                     
+                    author_number+=1
+                    
+                if article_title in article_titles:
+                    article_titles[article_title] = this_paper_authors
+
+                num_of_article+=1
+
+    logging.info(f'Done for Searching today !!')
+
+    if article_titles:
+        alert_mail(EMAIL_ADDR=EMAIL_ADDR, EMAIL_PASSWORD=EMAIL_PASSWORD, articles =article_titles)
 
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%Y-%m-%d, %H: %M: %S")
 
-    author_name ='Eugene A. Eliseev'
+    req_author_names =['Vojtěch Chlan', 'Antti Penttilä',  'Mehraneh Tirandari', 'Jiabin Yu', 'Kunpeng Dou', 'Zhengyan Darius Shi', 'Xiulin Ruan', 'Philip Kim', 'Romain Danneau',
+                       'Klaus Ensslin']
+
+    req_authors = [req.replace(',', '').replace('.', '') for req in req_author_names ]
     EMAIL_ADDR = input("Please enter your gmail id : ")
     EMAIL_PASSWORD = getpass.getpass("please enter your gmail password : ")
 
 
-    # job = schedule.every(30).seconds.do(main, author_name, EMAIL_ADDR, EMAIL_PASSWORD)
+    # arxiv_connect(req_authors, EMAIL_ADDR, EMAIL_PASSWORD)
+    # job = schedule.every().day.at("02:00").do(arxiv_connect, req_authors, EMAIL_ADDR, EMAIL_PASSWORD)
 
-    job = schedule.every().day.at("00:42").do(main, author_name, EMAIL_ADDR, EMAIL_PASSWORD)
+    job = schedule.every(10).seconds.do(arxiv_connect, req_authors, EMAIL_ADDR, EMAIL_PASSWORD)
     
     while True:
         schedule.run_pending()
